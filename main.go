f5386b62e3f774735fe2155eb5edc81e085208c5
Iswanul Umam <iswanul.umam@gmail.com>
2019-07-23 15:13:38 +0700

    query params

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
+
 import (
-    "net/http"
-    "github.com/labstack/echo"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func controllerUserWitQueryParam(c echo.Context) error {
-    // url param
-    urlParam := c.Param("name_param")
-    // query param
-    ageUser := c.QueryParam("age")
-    return c.JSON(http.StatusOK, map[string]string{
-        "name": urlParam,
-        "age":  ageUser,
-    })
+func UserSearchController(c echo.Context) error {
+	// get data from query param
+	match := c.QueryParam("match")
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"match":  match,
+		"result": []string{"adi", "aan", "asif"}, // hardcode data
+	})
 }
 
 func main() {
-    e := echo.New()
-    e.GET("/user/:name_param", controllerUserWitQueryParam)
-    e.Start(":8000")
+	e := echo.New()
+	// routing with query parameter
+	e.GET("/users", UserSearchController)
+
+	e.Start(":8000")
 }
