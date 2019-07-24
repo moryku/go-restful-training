f838164e7c8d385d3995047c1b7a4c2972aa2827
Iswanul Umam <iswanul.umam@gmail.com>
2019-07-24 10:08:13 +0700

    controller

--- routes/routes.go
+++ routes/routes.go
@@ -1,30 +1,30 @@
 package routes
 
 import (
-	"github.com/iswanulumam/go-restful-training/controllers"
+	c "github.com/iswanulumam/go-restful-training/controllers"
 	"github.com/labstack/echo"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
 	// hello routing
-	e.GET("/api/hello", controllers.HelloController)
+	e.GET("/api/hello", c.HelloController)
 
 	// user routing
-	e.GET("/api/users", controllers.GetUsersController)
-	e.GET("/api/users/:id", controllers.GetUserByIdController)
-	e.GET("/api/users", controllers.GetUserLikeControlller)
-	e.POST("/api/users", controllers.CreateUserController)
-	e.DELETE("/api/users/:id", controllers.DeleteUserController)
-	e.PUT("/api/users/:id", controllers.UpdateUserController)
+	e.GET("/api/users", c.GetUsersController)
+	e.GET("/api/users/:id", c.GetUserByIdController)
+	e.GET("/api/users", c.GetUserLikeControlller)
+	e.POST("/api/users", c.CreateUserController)
+	e.DELETE("/api/users/:id", c.DeleteUserController)
+	e.PUT("/api/users/:id", c.UpdateUserController)
 
 	// book routing
-	e.GET("/api/books", controllers.GetBooksController)
-	e.GET("/api/books/:id", controllers.GetBookByIdController)
-	e.GET("/api/books", controllers.GetBookLikeController)
-	e.POST("/api/books", controllers.CreateBookController)
-	e.DELETE("/api/books/:id", controllers.DeleteBookController)
-	e.PUT("/api/books/:id", controllers.UpdateBookController)
+	e.GET("/api/books", c.GetBooksController)
+	e.GET("/api/books/:id", c.GetBookByIdController)
+	e.GET("/api/books", c.GetBookLikeController)
+	e.POST("/api/books", c.CreateBookController)
+	e.DELETE("/api/books/:id", c.DeleteBookController)
+	e.PUT("/api/books/:id", c.UpdateBookController)
 
 	return e
 }
