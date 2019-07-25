5cf54eb13be7045c91e6893d147d3d5ed6fc1e31
Iswanul Umam <iswanul.umam@gmail.com>
2019-07-25 09:55:15 +0700

    implement middlewares

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"github.com/iswanulumam/go-restful-training/models"
 	"github.com/iswanulumam/go-restful-training/routes"
+
+	m "github.com/iswanulumam/go-restful-training/middlewares"
 )
 
 func main() {
 	e := routes.New()
+
+	// implement middleware with
+	m.LogMiddlewares(e)
 
 	// init db connection
 	models.InitDB("root:root123@/go_db?charset=utf8&parseTime=True&loc=Local")
