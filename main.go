785dd6bab1a36d8e18b31cc31e47e1079e390189
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-01 15:42:06 +0700

    add password table user

--- main.go
+++ main.go
@@ -18,8 +18,9 @@ var (
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name" form:"name"`
-	Email string `json:"email" form:"email"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
 
 func InitDB(connectionString string) {
