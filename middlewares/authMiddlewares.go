cfe19f49ecd0710f22310c85a273cbe0a95f96d8
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-01 18:10:55 +0700

    add basic auth

---
+++ middlewares/authMiddlewares.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo"
+)
+
+func BasicAuth(username, password string, c echo.Context) (bool, error) {
+	if username == "joe" && password == "secret" {
+		return true, nil
+	}
+	return false, nil
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,10 @@ package routes
 
 import (
 	c "github.com/iswanulumam/go-restful-training/controllers"
+	auth "github.com/iswanulumam/go-restful-training/middlewares"
+
 	"github.com/labstack/echo"
+	m "github.com/labstack/echo/middleware"
 )
 
 func New() *echo.Echo {
@@ -17,12 +20,12 @@ func New() *echo.Echo {
 	e.PUT("/api/users/:id", c.UpdateUserController)
 
 	// book routing
-	e.GET("/api/books", c.GetBooksController)
-	e.GET("/api/books/:id", c.GetBookController)
-	e.GET("/api/books", c.GetBooksLikeController)
-	e.POST("/api/books", c.CreateBookController)
-	e.DELETE("/api/books/:id", c.DeleteBookController)
-	e.PUT("/api/books/:id", c.UpdateBookController)
+	e.GET("/api/books", c.GetBooksController, m.BasicAuth(auth.BasicAuth))
+	e.GET("/api/books/:id", c.GetBookController, m.BasicAuth(auth.BasicAuth))
+	e.GET("/api/books", c.GetBooksLikeController, m.BasicAuth(auth.BasicAuth))
+	e.POST("/api/books", c.CreateBookController, m.BasicAuth(auth.BasicAuth))
+	e.DELETE("/api/books/:id", c.DeleteBookController, m.BasicAuth(auth.BasicAuth))
+	e.PUT("/api/books/:id", c.UpdateBookController, m.BasicAuth(auth.BasicAuth))
 
 	return e
 }
