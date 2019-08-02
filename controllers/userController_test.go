741ca2233c807ef54e1a0e7d6fa3b23eea1befcf
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-02 22:44:01 +0700

    finish mock http

--- controllers/userController_test.go
+++ controllers/userController_test.go
@@ -5,13 +5,11 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	m "github.com/iswanulumam/go-restful-training/models"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUsersController(t *testing.T) {
-	m.InitDB("root:root123@/crud_go?charset=utf8&parseTime=True&loc=Local")
 	e := echo.New()
 
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
