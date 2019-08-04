45dd92181f225c1e133a548f6d393569c8aecf79
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-04 18:59:39 +0700

    test user controller

--- controllers/userController_test.go
+++ controllers/userController_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gavv/httpexpect"
+	"github.com/iswanulumam/go-restful-training/routes"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,4 +24,18 @@ func TestGetUsersController(t *testing.T) {
 	if assert.NoError(t, h) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
+}
+
+func TestGetUsers(t *testing.T) {
+	// call all routes
+	router := routes.New()
+	// // create server based on router
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	// crate server app
+	app := httpexpect.New(t, server.URL)
+	app.GET("/api/users").
+		Expect().
+		Status(http.StatusOK)
 }
