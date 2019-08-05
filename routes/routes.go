abab538027f231aec1716fe7d2d2f88f99f0bf7c
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-05 16:33:33 +0700

    structuring user

--- routes/routes.go
+++ routes/routes.go
@@ -16,13 +16,5 @@ func New() *echo.Echo {
 	e.DELETE("/api/users/:id", c.DeleteUserController)
 	e.PUT("/api/users/:id", c.UpdateUserController)
 
-	// book routing
-	e.GET("/api/books", c.GetBooksController)
-	e.GET("/api/books/:id", c.GetBookController)
-	e.GET("/api/books", c.GetBooksLikeController)
-	e.POST("/api/books", c.CreateBookController)
-	e.DELETE("/api/books/:id", c.DeleteBookController)
-	e.PUT("/api/books/:id", c.UpdateBookController)
-
 	return e
 }
