d8a1a67551176617e64082317ab2581c131cb61d
Iswanul Umam <iswanul.umam@gmail.com>
2019-08-09 21:13:21 +0700

    finish add create test

--- controllers/userController_test.go
+++ controllers/userController_test.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
@@ -21,5 +23,31 @@ func TestGetUsersController(t *testing.T) {
 
 	if assert.NoError(t, h) {
 		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
+
+func TestCreateUserController(t *testing.T) {
+	e := echo.New()
+
+	data := `{"name":"iswanul", "email":"admin@admin.com", "password":"admin"}`
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(data))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	// c.SetPath("/api/users")
+	h := CreateUserController(c)
+
+	if assert.NoError(t, h) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		var jsonBody interface{}
+
+		json.NewDecoder(rec.Body).Decode(&jsonBody)
+		jsonMap := jsonBody.(map[string]interface{})
+		assert.Equal(t, "iswanul", jsonMap["name"].(string))
+		assert.Equal(t, "admin@admin.com", jsonMap["email"].(string))
+		assert.Equal(t, "admin", jsonMap["password"].(string))
 	}
 }
