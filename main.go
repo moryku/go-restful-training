2ea2c4ff2f296d4efa1236f0a70b4f5366ccdad5
Remap Development <mohammadrizkyk@gmail.com>
2019-10-16 09:31:58 +0700

    port

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
+	"os"
 	m "restful_training/middlewares"
 	"restful_training/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	e := routes.New()
 	// implement middleware with
 	m.LogMiddlewares(e)
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-	e.Start(":8000")
+	e.Logger.Fatal(e.Start(":" + port))
 }
